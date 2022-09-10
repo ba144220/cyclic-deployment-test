e72ddb1a179d3c1658194d1205eeb39c7a66f8c1
ba144220 <hs41511@gmail.com>
2022-09-10 20:56:20 +0800

    add some routes

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
+  console.log("GET /");
   return res.status(200).send("This is a home page");
+});
+app.get("/port", (req, res) => {
+  console.log("GET /port");
+  return res.status(200).send("Server listening on port: " + port);
+});
+
+app.post("/echo", (req, res) => {
+  console.log("POST /echo");
+  const data = req.body;
+  if (!data) {
+    return res.status(300).send("No data!!!");
+  }
+
+  return res.status(200).json(data);
 });
 
 app.listen(port, () => console.log("Server listening on port: " + port));
